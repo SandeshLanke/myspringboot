6f3982efd2107209a5a1968e04d90ee88d5f945b
Sandesh Lanke <lankesandesh2003@gmail.com>
2026-05-25 15:14:38 +0530

    Clear all the unnecessary files

--- src/main/java/com/springbootproject/myspringboot/MyspringbootApplication.java
+++ src/main/java/com/springbootproject/myspringboot/MyspringbootApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyspringbootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyspringbootApplication.class, args);
+
+        SpringApplication.run(MyspringbootApplication.class, args);
+        System.out.println("\n             Application is running...............");
 	}
 
 }
---
+++ src/main/java/com/springbootproject/myspringboot/QuestionController.java
@@ -0,0 +1,4 @@
+package com.springbootproject.myspringboot;
+
+public class QuestionController {
+}
